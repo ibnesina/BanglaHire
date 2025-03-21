93dadc6a8a6e19d6ef3f0ad043a394b403ceaa14
pantho-haque <pantho.haque.dev@gmail.com>
2025-03-22 01:56:02 +0600

    navbar design-frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,21 @@
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
+
 export default function Home() {
-  return <div>okay</div>;
+  return (
+    <div>
+      <NavBar />
+      <div className="flex justify-center items-center h-screen bg-slate-100">
+        <div className="flex flex-col items-center">
+          <Image src="/logo.png" width={100} height={100} />
+          <h1 className="text-4xl font-bold text-center">
+            Welcome to BanglaHire
+          </h1>
+          <p className="text-lg text-center">
+            The best place to find Bangladeshi talent
+          </p>
+        </div>
+      </div> 
+    </div>
+  );
 }
