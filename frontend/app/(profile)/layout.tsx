fbce1e09fd118c7751711f2dde3b14add1a9a873
pantho-haque <pantho.haque.dev@gmail.com>
2025-04-29 23:52:39 +0600

    restructuring the project done

--- frontend/app/(profile)/layout.tsx
+++ frontend/app/(profile)/layout.tsx
@@ -2,6 +2,8 @@
 import userStore from "@/lib/store";
 import { redirect } from "next/navigation";
 import "../globals.css";
+import NavBar from "@/components/navComponents/NavBar";
+import Footer from "@/components/Footer";
 
 export default function Layout({
   children,
@@ -11,5 +13,11 @@ export default function Layout({
   const user = userStore.user;
 
   if (user === null) redirect("/signin");
-  return <div>{children}</div>;
+  return <div>
+    <NavBar />
+    <div className="w-[60%] mx-auto py-8 px-4">
+    {children}
+    </div>
+    <Footer />
+    </div>;
 }
