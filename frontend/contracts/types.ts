4a052df228096fafe022b52dee53a0145a132d2b
pantho-haque <pantho.haque.dev@gmail.com>
2025-04-30 20:48:47 +0600

    listing the withdraw done

--- frontend/contracts/types.ts
+++ frontend/contracts/types.ts
@@ -1,9 +1,25 @@
+import { User } from "./users";
+
 export interface TCategoryWithMatrix {
-    id: number
-    name: string
-    skills: string[]
-    created_at: string
-    updated_at: string
-    skill_count: number
-    avg_rating: number
-  }
+  id: number;
+  name: string;
+  skills: string[];
+  created_at: string;
+  updated_at: string;
+  skill_count: number;
+  avg_rating: number;
+}
+
+export interface WithdrawalRequest {
+  id: string;
+  user_id: string;
+  amount: string;
+  gateway: string;
+  payment_details: {
+    account_no: string;
+  };
+  status: string;
+  created_at: string;
+  updated_at: string;
+  user: User;
+}
