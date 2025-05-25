4db00dc649f7a1cfa4f4e916d0dd433d21f911e6
MachangDoniel <donieltripura121@gmail.com>
2025-05-25 21:58:29 +0600

    modified navBar

--- frontend/components/navComponents/NavBar.tsx
+++ frontend/components/navComponents/NavBar.tsx
@@ -14,7 +14,7 @@ const NavBar = observer(() => {
     { path: "/talent", label: "Hire Talent", visibility: user?.type === "Client" },
     { path: "/work", label: "Find Work", visibility: user?.type === "Freelancer" },
     { path: "/about", label: "About Us", visibility: true },
-    { path: "/chat", label: "Chat", visibility: true },
+    { path: "/chat", label: "Chat", visibility: user?.type === "Client" || user?.type === "Freelancer" },
   ], [user?.type]);
 
 
